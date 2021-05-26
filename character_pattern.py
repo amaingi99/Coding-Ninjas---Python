c4445e20961421dfd65072114e74eef21d5dc27a
Aastha Maingi <aasthamaingi@gmail.com>
2021-05-26 20:46:20 +0530

    Create character_pattern.py
    Print the following pattern for the given N number of rows.
    Pattern for N = 4
    A
    BC
    CDE
    DEFG

---
+++ character_pattern.py
@@ -0,0 +1,14 @@
+## Read input as specified in the question
+## Print the required output in given format
+n = int(input())
+i = 1
+while i<=n:
+    p = i
+    j = 1
+    while j <=i:
+        charP = chr(ord('A')+p-1)
+        print(charP, end='')
+        p = p+1
+        j = j+1
+    print()
+    i = i+1
